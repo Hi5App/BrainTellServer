7c0900c45b2e6adb367f037478548dff9f8c0570
BabaBoogey <49062842+BabaBoogey@users.noreply.github.com>
2022-02-14 15:03:49 +0800

    add table TEffectSoma

--- models/models.go
+++ models/models.go
@@ -14,6 +14,19 @@ type TAnotation struct {
 	Mtime     time.Time `xorm:"not null default CURRENT_TIMESTAMP TIMESTAMP updated"`
 }
 
+type TEffectSoma struct {
+	Id        int       `xorm:"not null pk autoincr INT"`
+	Name      string    `xorm:"not null unique VARCHAR(100)"`
+	X         string    `xorm:"not null DECIMAL(10,3)"`
+	Y         string    `xorm:"not null DECIMAL(10,3)"`
+	Z         string    `xorm:"not null DECIMAL(10,3)"`
+	Imageid   string    `xorm:"not null index VARCHAR(100)"`
+	From      int       `xorm:"not null comment('来源 0:来自t_somainfo') INT"`
+	Isdeleted int       `xorm:"not null default 0 INT"`
+	Ctime     time.Time `xorm:"not null default CURRENT_TIMESTAMP TIMESTAMP created"`
+	Mtime     time.Time `xorm:"not null default CURRENT_TIMESTAMP TIMESTAMP updated"`
+}
+
 type TImage struct {
 	Id        int       `xorm:"not null pk autoincr INT"`
 	Name      string    `xorm:"not null unique VARCHAR(100)"`

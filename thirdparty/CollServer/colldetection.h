e75de9cdab19fd95dbd0c95bff1551a2953516a4
zackzhy <hello@zackzhy.cn>
2023-07-31 12:01:09 +0800

    新增排序和设置swc半径

--- thirdparty/CollServer/colldetection.h
+++ thirdparty/CollServer/colldetection.h
@@ -14,6 +14,7 @@
 #include <QJsonObject>
 #include <QJsonDocument>
 #include <algorithm>
+#include <cmath>
 
 class CollServer;
 class CollDetection : public QObject
@@ -33,12 +34,15 @@ public:
     XYZ getSomaCoordinate(QString apoPath);
     vector<NeuronSWC> specStructsDetection(V_NeuronSWC_list inputSegList, double dist_thresh=3);
     vector<NeuronSWC> tipDetection(V_NeuronSWC_list inputSegList, double dist_thresh=20);
-    vector<NeuronSWC> crossingDetection(V_NeuronSWC_list inputSegList);
+    vector<vector<NeuronSWC>> crossingDetection(V_NeuronSWC_list inputSegList, map<string, vector<string>> &parentsDict, map<string, vector<string>> &offspringsDict);
     void handleMulFurcation(vector<NeuronSWC>& outputSpecialPoints, int& count);
     void handleLoop(vector<NeuronSWC>& outputSpecialPoints, int& count);
     void handleNearBifurcation(vector<NeuronSWC>& bifurPoints, int& count);
     void handleTip(vector<NeuronSWC>& tipPoints);
-    void handleCrossing(vector<NeuronSWC>& crossingPoints);
+    void handleCrossing(vector<vector<NeuronSWC>>& crossingPoints, map<string, vector<string>> &parentsDict, map<string, vector<string>> &offspringsDict);
+
+    void sortSWC(QString fileOpenName, QString fileSaveName, double thres=1000000000, V3DLONG rootid=1000000000);
+    void setSWCRadius(QString filePath, int r);
 
 public slots:
     void detectOthers();
